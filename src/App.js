06f25e114c8294299c4a62404917088746c3de18
Pierralinouie <pa.gauthier@inouiedistribution.com>
2024-12-24 16:26:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-50 py-8">
       <div className="container mx-auto">
-        <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">
-          ADN Digital Form
-        </h1>
         <ADNDigitalForm />
       </div>
     </div>
