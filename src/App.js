dadafebee4128ea9e3722ed6e4b9773f292019de
Pierralinouie <pa.gauthier@inouiedistribution.com>
2024-12-24 15:46:09 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ADNDigitalForm from './components/ADNDigitalForm';
+
+function App() {
+  return (
+    <div className="App">
+      <ADNDigitalForm />
+    </div>
+  );
+}
+
+export default App;
